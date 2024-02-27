aba4ff53ec081e6cb831b49cbeec3c33386ff0ed
Pitak Mennakret <pitakmennakret@MacBook-Pro-Pxt.local>
2024-02-27 19:30:21 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// index.js
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('static'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
