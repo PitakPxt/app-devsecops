1211436474c856b0d5587df4b90f9324ad682916
Pitak Mennakret <pitakmennakret@MacBook-Pro-Pxt.local>
2024-02-27 22:47:44 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
-import { Hono } from "hono";
+// import { serve } from "@hono/node-server";
+// import { serveStatic } from "@hono/node-server/serve-static";
+// import { Hono } from "hono";
 
-const app = new Hono();
+// const app = new Hono();
 
-app.use("/static/*", serveStatic({ root: "./" }));
-app.get("/", serveStatic({ root: "./static/" }));
+// app.use("/static/*", serveStatic({ root: "./" }));
+// app.get("/", serveStatic({ root: "./static/" }));
 
-const port = 3000;
-console.log(`Server is running on port ${port}`);
+// const port = 3000;
+// console.log(`Server is running on port ${port}`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+// serve({
+//   fetch: app.fetch,
+//   port,
+// });
